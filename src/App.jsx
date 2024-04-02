c75293c336f9014b949df009b7d8807a340205a1
Abhinav Sinha <100909247+Abhinav01Sinha@users.noreply.github.com>
2024-04-02 23:00:27 +0530

    updated projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,40 @@
-import './App.css'
-import Navbar from "./Components/Navbar"
-import MySkills from "./Components/Skills/index.jsX"
-import Education from "./Components/Education"
-import Contact from './Components/Contact/index.jsx'
-import Experience from './Components/Experience/index.jsx'
-import HeroSection from "./Components/HeroSection/index.jsx"
-import Projects from './Components/Projects/index.jsx'
-import Footer from './Components/Footer/index.jsx'
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Education from "./Components/Education";
+import Contact from "./Components/Contact/index.jsx";
+import Experience from "./Components/Experience/index.jsx";
+import HeroSection from "./Components/HeroSection/index.jsx";
+import Projects from "./Components/Projects/index.jsx";
+import Footer from "./Components/Footer/index.jsx";
+import Skills from "./Components/Skills/index.jsx";
 
 import { ThemeProvider } from "styled-components";
-import { useState, useEffect } from "react";
-import { darkTheme, lightTheme } from './utils/Themes.js'
-import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { useState } from "react";
+import { darkTheme } from "./utils/themes.jsx";
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
 import styled from "styled-components";
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
   width: 100%;
   overflow-x: hidden;
-`
+`;
 
 const Wrapper = styled.div`
-  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
   width: 100%;
-  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
-`
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
@@ -33,35 +42,37 @@ function App() {
 
   return (
     <>
-      <ThemeProvider theme = {darkTheme}>
+      <ThemeProvider theme={darkTheme}>
         <Router>
-          <Navbar/>
+          <Navbar />
           <Body>
             <HeroSection />
             <Wrapper>
-              <MySkills/>
-              <Experience/>
+              <Skills />
+              <Experience />
             </Wrapper>
 
             <Wrapper>
-              <Projects openModal={openModal} setOpenModal={setOpenModal}/>
-              <Education/>              
+              <Projects openModal={openModal} setOpenModal={setOpenModal} />
+              <Education />
             </Wrapper>
 
             <Wrapper>
-              <Contact/>
+              <Contact />
             </Wrapper>
 
             <Footer />
-            {/* {openModal.state &&
-              <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
-            } */}
+            {/* {openModal.state && (
+              <ProjectDetails
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
+            )} */}
           </Body>
-
-        </Router>           
-      </ThemeProvider>   
+        </Router>
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App ;
+export default App;
